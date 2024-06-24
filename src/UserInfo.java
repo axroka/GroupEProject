478a7393f4c80e8fa44765c9afbb4efd59f7c5ec
Dt3a1 <Dnguye12@umbc.edu>
2024-06-24 15:56:12 -0400

    Update UserInfo.java

--- src/UserInfo.java
+++ src/UserInfo.java
@@ -8,15 +8,21 @@ class UserInfo {
 
         System.out.println("What is your name?");
         String name = scanner.nextLine();
-
+        
+        System.out.println("How old are you?");
+        int age = Integer.parseInt(scanner.nextLine());
+                
+        if (age < 18) {
+            System.out.println("Sorry you must be atleast 18 years old to rent a vehicle");
+        } else {
+            System.out.println("Please present your ID");
+        }
+        
         System.out.println("What type of vehicle would you like to rent?");
         String vehicle = scanner.nextLine();
 
         System.out.println("How long are you planning to rent this vehicle?");
         double time = Double.parseDouble(scanner.nextLine());
-
-        System.out.println("How old are you?");
-        int age = Integer.parseInt(scanner.nextLine());
 
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
@@ -25,10 +31,6 @@ class UserInfo {
 
 
 
-        if (age < 18) {
-            System.out.println("Sorry you must be atleast 18 years old to rent a vehicle");
-        } else {
-            System.out.println("Please present your ID");
-        }
+
     }
 }
