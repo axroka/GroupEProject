48c6ce5264753b29a7396c71d901cd59757c4fe5
DT <ddtng02@gmail.com>
2024-06-29 00:17:42 -0400

    fixing userinfo

--- src/UserInfo.java
+++ src/UserInfo.java
@@ -15,26 +15,13 @@ class UserInfo {
         System.out.println("How old are you?");
         age = Integer.parseInt(scanner.nextLine());
 
-        if (age < 25) {
-            System.out.println("Sorry you must be at least 25 years old to rent a vehicle");
+        if (age < 18) {
+            System.out.println("Sorry you must be at least 18 years old to rent a vehicle");
             scanner.close();
             return false;
         }
-        System.out.println("Do you have a driver license");
-        String License = scanner.nextLine().toLowerCase();
-        if(License.equals("yes")){
-            System.out.println("Do you have insurance");
-            String Insurance = scanner.nextLine().toLowerCase();
-            if(License.equals("yes") && Insurance.equals("yes")){
-                System.out.println("What type of vehicle would you like to rent?");
-                vehicleType = scanner.nextLine().toLowerCase();
-            } else{
-                System.out.println("Sorry you are intelligible to rent out a vehicle please have a nice day");
-                scanner.close();
-                return false;
-            }
-        }
-
+        System.out.println("What type of vehicle would you like to rent?");
+        vehicleType = scanner.nextLine().toLowerCase();
 
         rentalDays = getRentalDays(scanner);
         System.out.println("Name: " + name);
@@ -44,26 +31,20 @@ class UserInfo {
         scanner.close();
         return true;
     }
-        private int getRentalDays(Scanner scanner) {
-            System.out.println("How many days are you planning to rent this vehicle?");
-            int days = Integer.parseInt(scanner.nextLine());
+    private int getRentalDays(Scanner scanner) {
+        System.out.println("How many days are you planning to rent this vehicle?");
+        int days = Integer.parseInt(scanner.nextLine());
 
-            if (days < 0) {
-                System.out.println("Invalid input");
-                return getRentalDays(scanner);
-            } else {
-                return days;
-            }
+        if (days < 0) {
+            System.out.println("Invalid input");
+            return getRentalDays(scanner);
+        } else {
+            return days;
         }
-        public String getVehicleType(){
+    }
+    public String getVehicleType(){
         return vehicleType;
     }
-
-    public int getRentalDays() {
-        return rentalDays;
-    }
-}
-
 
     public int getRentalDays() {
         return rentalDays;
